7ae9d176abbf3bf80b0eea30266fe1917dcd7046
ayazsin <a.ayazsin@gmail.com>
2021-09-12 12:40:47 +0200

    first

---
+++ src/main/java/qui/search/ApiNotes.java
@@ -0,0 +1,17 @@
+package qui.search;
+
+public class ApiNotes {
+
+    /*
+            API: Application Programming Interface
+                 APIs do not have user interface
+            APIs are used to communicate with other applications
+            CRUD - Create, Read, Update, Delete
+
+     */
+
+
+
+
+
+}
